1e6bc4804d526c6e8ac835eb08ef0858cd58432b
Himmat Paliwal <himmat.paliwal@indianic.com>
2021-05-27 16:28:57 +0530

    Copyright text update

--- Controller/Adminhtml/Product/Delete.php
+++ Controller/Adminhtml/Product/Delete.php
@@ -1,9 +1,37 @@
 <?php
+/**
+ * MagentoCoders
+ * Copyright (C) 2021 MagentoCoders <info@magentocoders.com>
+ *
+ * NOTICE OF LICENSE
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://opensource.org/licenses/gpl-3.0.html.
+ *
+ * Do not edit or add to this file if you wish to upgrade this extension to newer
+ * version in the future.
+ *
+ * @category    MagentoCoders
+ * @package     MagentoCoders_CustomCatalog
+ * @copyright   Copyright (C) 2021 MagentoCoders (http://www.magentocoders.com/)
+ * @license     http://www.magentocoders.com/license-agreement.html
+ * @author      MagentoCoders <info@magentocoders.com>
+ */
+
 namespace MagentoCoders\CustomCatalog\Controller\Adminhtml\Product;
 
 use Magento\Backend\App\Action;
 use Magento\Backend\App\Action\Context;
 
+/**
+ * Class Delete
+ * @package MagentoCoders\CustomCatalog\Controller\Adminhtml\Product
+ */
 class Delete extends Action
 {
     /**
--- Model/ResourceModel/Product.php
+++ Model/ResourceModel/Product.php
@@ -1,8 +1,31 @@
 <?php
+/**
+ * MagentoCoders
+ * Copyright (C) 2021 MagentoCoders <info@magentocoders.com>
+ *
+ * NOTICE OF LICENSE
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://opensource.org/licenses/gpl-3.0.html.
+ *
+ * Do not edit or add to this file if you wish to upgrade this extension to newer
+ * version in the future.
+ *
+ * @category    MagentoCoders
+ * @package     MagentoCoders_CustomCatalog
+ * @copyright   Copyright (C) 2021 MagentoCoders (http://www.magentocoders.com/)
+ * @license     http://www.magentocoders.com/license-agreement.html
+ * @author      MagentoCoders <info@magentocoders.com>
+ */
+
 namespace MagentoCoders\CustomCatalog\Model\ResourceModel;
 
 use Magento\Framework\Model\ResourceModel\Db\AbstractDb;
-
 
 /**
  * Class Product
